09edda4836792263f57645a17f4184090a097226
Matt Mackay <mattem@gmail.com>
2015-04-21 18:33:15 +0100

    Using FormationField

--- src/main/java/me/mattem/formation/calibration/FormationStringCalibrationClass.java
+++ src/main/java/me/mattem/formation/calibration/FormationStringCalibrationClass.java
@@ -1,11 +1,18 @@
 package me.mattem.formation.calibration;
 
+import me.mattem.formation.annotations.FormationField;
+import me.mattem.formation.annotations.FormationFieldType;
 import me.mattem.formation.annotations.FormationInclude;
 
 @FormationInclude
 public class FormationStringCalibrationClass {
 	private String simpleString;
-	private String simpleStringTwo;
+	private String simpleRequiredString;
+	private String overrideString;
+	private String color;
+	private String dateTime;
+	private String week;
+	private String textArea;
 	
 	public String getSimpleString() {
 		return simpleString;
@@ -13,10 +20,47 @@ public class FormationStringCalibrationClass {
 	public void setSimpleString(String simpleString) {
 		this.simpleString = simpleString;
 	}
-	public String getSimpleStringTwo() {
-		return simpleStringTwo;
+	@FormationField(required=true, views={"RequiredFields", "SimpleProperties"})
+	public String getSimpleRequiredString() {
+		return simpleRequiredString;
 	}
-	public void setSimpleStringTwo(String simpleStringTwo) {
-		this.simpleStringTwo = simpleStringTwo;
+	public void setSimpleRequiredString(String simpleRequiredString) {
+		this.simpleRequiredString = simpleRequiredString;
 	}
+	@FormationField(overrideAs=FormationStringCalibrationClass.class)
+	public String getOverrideString() {
+		return overrideString;
+	}
+	public void setOverrideString(String overrideString) {
+		this.overrideString = overrideString;
+	}
+	@FormationField(fieldType=FormationFieldType.COLOR)
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	@FormationField(fieldType=FormationFieldType.DATETIME_LOCAL)
+	public String getDateTime() {
+		return dateTime;
+	}
+	public void setDateTime(String dateTime) {
+		this.dateTime = dateTime;
+	}
+	@FormationField(fieldType=FormationFieldType.WEEK)
+	public String getWeek() {
+		return week;
+	}
+	public void setWeek(String week) {
+		this.week = week;
+	}
+	@FormationField(fieldType=FormationFieldType.TEXT_AREA)
+	public String getTextArea() {
+		return textArea;
+	}
+	public void setTextArea(String textArea) {
+		this.textArea = textArea;
+	}
+
 }
